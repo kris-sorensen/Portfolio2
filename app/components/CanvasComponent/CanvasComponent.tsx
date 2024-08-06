220f3aea225a018db4a6af3e9181cbe105651e68
Kris Sorensen <Krismsorensen@gmail.com>
2024-08-06 16:27:25 -0600

    Floor laid

--- app/components/CanvasComponent/CanvasComponent.tsx
+++ app/components/CanvasComponent/CanvasComponent.tsx
@@ -1,18 +1,41 @@
 "use client";
-import React, { Suspense } from "react";
-import * as three from "three";
-import { Loader } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useState } from "react";
+import * as THREE from "three";
+import { Environment, Loader, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import Scene from "./Scene";
+
+function Rig() {
+  const [vec] = useState(() => new THREE.Vector3());
+  const { camera, mouse } = useThree();
+  useFrame(() => camera.position.lerp(vec.set(mouse.x * 2, 1, 60), 0.05));
+  return null;
+  // return <CameraShake maxYaw={0.01} maxPitch={0.01} maxRoll={0.01} yawFrequency={0.5} pitchFrequency={0.5} rollFrequency={0.4} />
+}
 
 const CanvasComponent = () => {
   console.log(`render Canvas comp`);
   return (
     <div className="absolute top-[0px] left-0 w-full h-full outline-none">
-      <Canvas linear dpr={[1, 2]}>
+      <Canvas
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [0, 160, 160], fov: 20 }}
+      >
+        <OrbitControls makeDefault />
+        <fog attach="fog" args={["lightpink", 60, 100]} />
         <Suspense fallback={null}>
           {/* <color attach="background" args={["#e1f5ff"]} /> */}
-          <Scene />
+          <Scene position={[-4.5, -4, 0]} />
+          <ambientLight intensity={0.5} />
+          <Rig />
+          {/* <Model position={[-4.5, -4, 0]} rotation={[0, -2.8, 0]} /> */}
+          <spotLight position={[50, 50, -30]} castShadow />
+          <pointLight position={[-10, -10, -10]} color="red" intensity={3} />
+          <pointLight position={[0, -5, 5]} intensity={0.5} />
+          <directionalLight position={[0, -5, 0]} color="red" intensity={2} />
+          {/* <Light /> */}
+          <Environment preset="warehouse" />
         </Suspense>
       </Canvas>
       <Loader />
--- app/components/CanvasComponent/Scene.tsx
+++ app/components/CanvasComponent/Scene.tsx
@@ -1,22 +1,55 @@
-import React from "react";
-import { Plane } from "@react-three/drei";
+import React, { useRef } from "react";
+import { Plane, Reflector } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import WelcomeText from "./Shaders/WelcomeText/WelcomeText";
 import BasicShaderMaterial from "./Shaders/BasicShader/BasicShaderMaterial";
+import * as THREE from "three";
 
-const FullScreenPlane = () => {
+const material = new THREE.MeshPhysicalMaterial({
+  color: new THREE.Color("#bb86a1").convertSRGBToLinear(),
+  roughness: 0,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const FullScreenPlane = (props) => {
+  const group = useRef();
   // * Make Plane full screen
   const { viewport } = useThree();
   const scale = Math.max(viewport.width, viewport.height);
 
   console.log(`render scene comp`);
   return (
-    <group position={[0, 0, 0]}>
-      <WelcomeText />
-      <Plane args={[scale, scale]}>
-        <BasicShaderMaterial />
-      </Plane>
-      {/* Scale the plane to cover the entire viewport */}
+    <group
+      ref={group}
+      {...props}
+      dispose={null}
+      onClick={() => console.log(group.current)}
+    >
+      {/* <Plane {...props} scale={20}> */}
+      {/* <ambientLight intensity={1} /> */}
+      <Reflector
+        resolution={1024}
+        receiveShadow
+        mirror={0}
+        mixBlur={1}
+        mixStrength={0.3}
+        depthScale={1}
+        minDepthThreshold={0.8}
+        maxDepthThreshold={1}
+        position={[0, 0, 8]}
+        scale={[2, 2, 1]}
+        rotation={[-Math.PI / 2, 0, Math.PI]}
+        args={[70, 70]}
+      >
+        {/* <meshPhysicalMaterial /> */}
+        {(Material, props) => (
+          <Material metalness={0.25} color="#eea6b1" roughness={1} {...props} />
+        )}
+      </Reflector>
+
+      {/* <meshBasicMaterial color="#b20e27" side={THREE.DoubleSide} /> */}
+      {/* </Plane> */}
     </group>
   );
 };
