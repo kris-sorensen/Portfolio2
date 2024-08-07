2ff87ddcef4c44159fb087a500a91bc863fe4c15
Kris Sorensen <Krismsorensen@gmail.com>
2024-08-07 14:48:14 -0600

    logo image added

--- app/components/CanvasComponent/components/Logo/Logo.tsx
+++ app/components/CanvasComponent/components/Logo/Logo.tsx
@@ -1,63 +1,75 @@
 import React, { useRef } from "react";
-import { Text } from "@react-three/drei";
+import { Text, Image } from "@react-three/drei";
 
 const Logo = () => {
   const text = useRef(null);
-  const circleColor = "#00BFFF"; // Light blue color (can be adjusted based on the exact color from the image)
+  const circleColor = "#56B1C5"; // Light blue color (can be adjusted based on the exact color from the image)
+  const imageWidth = 501;
+  const imageHeight = 282;
+  const aspectRatio = imageWidth / imageHeight;
+
   return (
-    <group position={[0, 3, 0]} scale={[1.6, 1.6, 1.6]}>
-      <mesh>
-        <Text
-          ref={text}
-          letterSpacing={0.005}
-          fontSize={2.3}
-          anchorX={"center"}
-          font="./fonts/dessau-heavy-regular.woff"
-        >
-          ZURCHERS
-        </Text>
-      </mesh>
-      <group position={[-0.3, -1.2, 0]} scale={[1, 1, 1]}>
-        <group position={[-3, 0, 0]}>
+    <group>
+      <Image
+        position={[-4.8, 5.65, 0]}
+        url="/images/logo.png"
+        scale={[aspectRatio * 3, 1 * 3]}
+      />
+      <group position={[0, 3, 0]} scale={[1.6, 1.6, 1.6]}>
+        <mesh>
           <Text
             ref={text}
             letterSpacing={0.005}
-            fontSize={0.6}
+            fontSize={2.3}
             anchorX={"center"}
-            font="./fonts/altehaasgroteskregular.woff"
+            font="./fonts/dessau-heavy-regular.woff"
           >
-            Party
+            <meshBasicMaterial color="#ffffff" />
+            ZURCHERS
           </Text>
-        </group>
-        <mesh position={[-1.95, 0, 0]}>
-          <circleGeometry args={[0.1, 16]} />
-          <meshBasicMaterial color={circleColor} />
         </mesh>
-        <group position={[-0.15, 0, 0]}>
-          <Text
-            ref={text}
-            letterSpacing={0.005}
-            fontSize={0.6}
-            anchorX={"center"}
-            font="./fonts/altehaasgroteskregular.woff"
-          >
-            Costumes
-          </Text>
-        </group>
-        <mesh position={[1.65, 0, 0]}>
-          <circleGeometry args={[0.1, 16]} />
-          <meshBasicMaterial color={circleColor} />
-        </mesh>
-        <group position={[3.2, 0, 0]}>
-          <Text
-            ref={text}
-            letterSpacing={0.005}
-            fontSize={0.6}
-            anchorX={"center"}
-            font="./fonts/altehaasgroteskregular.woff"
-          >
-            Wedding
-          </Text>
+        <group position={[-0.3, -1.2, 0]} scale={[1, 1, 1]}>
+          <group position={[-3, 0, 0]}>
+            <Text
+              ref={text}
+              letterSpacing={0.005}
+              fontSize={0.6}
+              anchorX={"center"}
+              font="./fonts/altehaasgroteskregular.woff"
+            >
+              Party
+            </Text>
+          </group>
+          <mesh position={[-1.95, 0, 0]}>
+            <circleGeometry args={[0.1, 16]} />
+            <meshBasicMaterial color={circleColor} />
+          </mesh>
+          <group position={[-0.15, 0, 0]}>
+            <Text
+              ref={text}
+              letterSpacing={0.005}
+              fontSize={0.6}
+              anchorX={"center"}
+              font="./fonts/altehaasgroteskregular.woff"
+            >
+              Costumes
+            </Text>
+          </group>
+          <mesh position={[1.65, 0, 0]}>
+            <circleGeometry args={[0.1, 16]} />
+            <meshBasicMaterial color={circleColor} />
+          </mesh>
+          <group position={[3.2, 0, 0]}>
+            <Text
+              ref={text}
+              letterSpacing={0.005}
+              fontSize={0.6}
+              anchorX={"center"}
+              font="./fonts/altehaasgroteskregular.woff"
+            >
+              Wedding
+            </Text>
+          </group>
         </group>
       </group>
     </group>
