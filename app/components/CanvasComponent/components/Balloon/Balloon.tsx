c30f21b7ec4d6bfba47b2287b139f282adb2736e
Kris Sorensen <Krismsorensen@gmail.com>
2024-08-09 11:27:39 -0600

    preload balloon asset added back in

--- app/components/CanvasComponent/components/Balloon/Balloon.tsx
+++ app/components/CanvasComponent/components/Balloon/Balloon.tsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import React, { useRef } from "react";
+import React, { useRef, memo } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 import { useFrame } from "@react-three/fiber";
@@ -15,87 +15,92 @@ function easeInCubicRoot(t: number): number {
   return Math.cbrt(t);
 }
 
-export function Balloon(
-  props: JSX.IntrinsicElements["group"] & {
+const Balloon = memo(
+  ({
+    color,
+    position,
+    onRemove,
+  }: {
     color: THREE.Color;
+    position: [number, number, number];
     onRemove: () => void;
+  }) => {
+    const { nodes } = useGLTF("/models/balloon-transformed.glb") as GLTFResult;
+    const mesh = useRef<THREE.Mesh>(null);
+    const scaleFactor = useRef(0);
+    const elapsedTime = useRef(0);
+    const growing = useRef(true);
+
+    const initialScale = 0.0;
+    const targetScale = 0.75;
+    const resetPoint = 30;
+
+    const delay = 0;
+
+    useFrame((state, delta) => {
+      if (!mesh.current) return;
+
+      elapsedTime.current += delta;
+
+      if (growing.current) {
+        if (elapsedTime.current > delay) {
+          scaleFactor.current = Math.min(scaleFactor.current + delta * 0.3, 1);
+
+          const easeFactor =
+            initialScale +
+            easeInCubicRoot(scaleFactor.current) * (targetScale - initialScale);
+
+          mesh.current.scale.set(easeFactor, easeFactor, easeFactor);
+
+          const initialY = 1.001;
+
+          if (!mesh.current.geometry.boundingBox) {
+            mesh.current.geometry.computeBoundingBox();
+          }
+
+          const boundingBoxHeight =
+            (mesh.current.geometry.boundingBox?.max.y ?? 0) -
+            (mesh.current.geometry.boundingBox?.min.y ?? 0);
+          mesh.current.position.y =
+            initialY + (easeFactor - 1) * (boundingBoxHeight / 2);
+
+          if (scaleFactor.current >= 1) {
+            growing.current = false;
+          }
+        }
+      } else {
+        mesh.current.position.y += delta * 0.5;
+
+        if (mesh.current.position.y > resetPoint) {
+          onRemove();
+        }
+      }
+    });
+
+    return (
+      <group position={position}>
+        <mesh
+          ref={mesh}
+          geometry={nodes.Line001.geometry}
+          scale={[0.001, 0.001, 0.001]}
+        >
+          <meshPhysicalMaterial
+            color={color}
+            roughness={0.2}
+            clearcoat={1}
+            clearcoatRoughness={0.1}
+            metalness={0.1}
+            reflectivity={1}
+            transparent={true}
+            opacity={0.9}
+            fog={false}
+          />
+        </mesh>
+      </group>
+    );
   }
-) {
-  const { nodes } = useGLTF("/models/balloon-transformed.glb") as GLTFResult;
-  const mesh = useRef<THREE.Mesh>(null);
-  const scaleFactor = useRef(0);
-  const elapsedTime = useRef(0);
-  const growing = useRef(true);
-
-  const initialScale = 0.0;
-  const targetScale = 0.75;
-  const resetPoint = 30;
-
-  const delay = 0;
-
-  useFrame((state, delta) => {
-    if (!mesh.current) return;
-
-    elapsedTime.current += delta;
-
-    if (growing.current) {
-      if (elapsedTime.current > delay) {
-        scaleFactor.current = Math.min(scaleFactor.current + delta * 0.3, 1);
-
-        const easeFactor =
-          initialScale +
-          easeInCubicRoot(scaleFactor.current) * (targetScale - initialScale);
-
-        mesh.current.scale.set(easeFactor, easeFactor, easeFactor);
-
-        const initialY = 1.001;
-
-        // Ensure the boundingBox is computed
-        if (!mesh.current.geometry.boundingBox) {
-          mesh.current.geometry.computeBoundingBox();
-        }
-
-        const boundingBoxHeight =
-          (mesh.current.geometry.boundingBox?.max.y ?? 0) -
-          (mesh.current.geometry.boundingBox?.min.y ?? 0);
-        mesh.current.position.y =
-          initialY + (easeFactor - 1) * (boundingBoxHeight / 2);
-
-        if (scaleFactor.current >= 1) {
-          growing.current = false;
-        }
-      }
-    } else {
-      mesh.current.position.y += delta * 0.5;
-
-      if (mesh.current.position.y > resetPoint) {
-        props.onRemove();
-      }
-    }
-  });
-
-  return (
-    <group {...props}>
-      <mesh
-        scale={0.001}
-        ref={mesh}
-        name="Line001"
-        geometry={nodes.Line001.geometry}
-      >
-        <meshPhysicalMaterial
-          color={props.color}
-          roughness={0.2}
-          clearcoat={1}
-          clearcoatRoughness={0.1}
-          metalness={0.1}
-          reflectivity={1}
-          transparent={true}
-          opacity={0.9}
-          fog={false}
-        />
-      </mesh>
-    </group>
-  );
-}
+);
 
 useGLTF.preload("/models/balloon-transformed.glb");
+
+export default Balloon;
